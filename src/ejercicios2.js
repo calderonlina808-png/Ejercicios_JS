47373cbf91eaa62f8474b965b0af156e77208d96
Lina Caldeón <calderonlina808@gmail.com>
2026-03-06 13:00:37 -0500

    feat: ejercicio 9

--- src/ejercicios2.js
+++ src/ejercicios2.js
@@ -140,6 +140,27 @@ function esMultiplo(num1, num2) {
 
 // Test 9:
 function obtenerEstacion(mes) {
+	switch (mes) {
+		case 12:
+			case 1:
+				case2: 
+				return "Invierno";
+				case 3:
+					case 4:
+						case 5:
+							return "Primavera"; 
+							case 6:
+								case 7: 
+								case 8:
+									return "Verano";
+									case 9:
+										case 10:
+											case 11:
+												return "Otoño";
+												default:
+													return "Mes inválido";
+
+	}
 	// Recibe un número del 1 al 12 representando el mes.
 	// Devuelve la estación del año (hemisferio norte) usando switch:
 	// - Diciembre, Enero, Febrero → "Invierno"
